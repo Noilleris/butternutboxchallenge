9074e6e61ee9371cbef15b491537a174ce6f2756
Alex Chervinsky <noilleris@gmail.com>
2021-08-03 16:18:39 +0100

    Feature: Exceptions added, rasing them for nil, empty and bad numbers

--- formatter.rb
+++ formatter.rb
@@ -1,14 +1,45 @@
 module Formatter
   module PhoneNumber
+    module Exceptions
+      class NilNumber < StandardError
+        def message
+          'Number could not be nil'
+        end
+      end
+
+      class EmptyNumber < StandardError
+        def message
+          'Number could not be nil'
+        end
+      end
+    end
+
     module UK
+      module Exceptions
+        class BadNumber < StandardError
+          def message
+            'Number in wong format, should be ((0)|(44)|(+44))7XXXXXXXXX, brackets, hyphens and whitespaces allowed'
+          end
+        end
+      end
+
       COUNTRY_CODE = '+44'.freeze
+      NORMILIZE_REGEX = /(\()|(\))|(\-)|(\s)/i.freeze
+      VALID_PREFIX_REGEX = /((^0)|(^\+?44))/.freeze
+      VALID_NORMAL_NUMBER = /^(0|(\+?44))7[0-9]{9}$/.freeze
 
       def normalize(number)
-        number.gsub(/(\()|(\))|(\-)|(\s)/i, '')
+        raise Formatter::PhoneNumber::Exceptions::NilNumber if number.nil?
+
+        raise Formatter::PhoneNumber::Exceptions::EmptyNumber if number.empty?
+
+        number.gsub(NORMILIZE_REGEX, '')
       end
 
       def validate(number)
-        number.gsub(/((^0)|(^\+?44))/, '')
+        raise Formatter::PhoneNumber::UK::Exceptions::BadNumber if number !~ VALID_NORMAL_NUMBER
+
+        number.gsub(VALID_PREFIX_REGEX, '')
       end
 
       def self.format(number)

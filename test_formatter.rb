6b0bf881e779b910400f20be81e83c8a835a1273
Alex Chervinsky <noilleris@gmail.com>
2021-08-03 16:19:07 +0100

    Test: Proper classes for exceptions applied

--- test_formatter.rb
+++ test_formatter.rb
@@ -35,43 +35,43 @@ class FormatterTest < Minitest::Test
 
   describe 'Formatter should be throw exception for' do
     it 'nil number' do
-      assert_raises NilNumber do
+      assert_raises Formatter::PhoneNumber::Exceptions::NilNumber do
         Formatter::PhoneNumber::UK.format(nil)
       end
     end
 
     it 'empty number' do
-      assert_raises EmptyNumber do
+      assert_raises Formatter::PhoneNumber::Exceptions::EmptyNumber do
         Formatter::PhoneNumber::UK.format('')
       end
     end
 
     it 'random string' do
-      assert_raises NotANumber do
+      assert_raises Formatter::PhoneNumber::UK::Exceptions::BadNumber do
         Formatter::PhoneNumber::UK.format('abcdefghijk')
       end
     end
 
     it 'short number 0712345678' do
-      assert_raises WrongLength do
+      assert_raises Formatter::PhoneNumber::UK::Exceptions::BadNumber do
         Formatter::PhoneNumber::UK.format('0712345678')
       end
     end
 
     it 'long number 071234567891011' do
-      assert_raises WrongLength do
+      assert_raises Formatter::PhoneNumber::UK::Exceptions::BadNumber do
         Formatter::PhoneNumber::UK.format('071234567891011')
       end
     end
 
     it 'corrupted number 071234RR789' do
-      assert_raises BadNumber do
+      assert_raises Formatter::PhoneNumber::UK::Exceptions::BadNumber do
         Formatter::PhoneNumber::UK.format('071234RR789')
       end
     end
 
     it 'number outside of the UK' do
-      assert_raises WrongCountry do
+      assert_raises Formatter::PhoneNumber::UK::Exceptions::BadNumber do
         Formatter::PhoneNumber::UK.format('+7 (123) 456-78-90')
       end
     end
